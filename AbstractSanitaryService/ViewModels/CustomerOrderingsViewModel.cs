9ff390a708eb91abf6d2a284ffd1d682df6e1b0b
220 <220@DESKTOP-VCLGFGE>
2018-06-06 15:50:04 +0400

    12W

---
+++ AbstractSanitaryService/ViewModels/CustomerOrderingsViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace AbstractSanitaryService.ViewModels
+{
+    public class CustomerOrderingsViewModel
+    {
+        public string CustomerName { get; set; }
+
+        public string DateCreate { get; set; }
+
+        public string ItemName { get; set; }
+
+        public int Count { get; set; }
+
+        public decimal Sum { get; set; }
+
+        public string Status { get; set; }
+    }
+}
